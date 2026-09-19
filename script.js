e4a09104f0f9a3023b9abfa36e32d85c89f55876
powerpokemon83-design <powerpokemon83@gmail.com>
2026-09-19 23:02:00 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+/* ================================
+   CURRENT YEAR
+================================ */
+
+const year = document.getElementById("year");
+
+if (year) {
+    year.textContent = new Date().getFullYear();
+}
